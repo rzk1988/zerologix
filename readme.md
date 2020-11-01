c059c8c553b0bd24f303a03e0fca2dbf5bafd63a
Ray Ren <ray@tix.com.au>
2020-11-01 20:01:01 +1100

    added simple test case for apis

--- readme.md
+++ readme.md
@@ -1,9 +1,20 @@
-## About the project
+# About the project
+Provides APIs for engage with Tweets
 
-# Main features
+## Main features
 - Post a tweet
 - Reply a tweet
 - Retweet a tweet
 - Star a tweet
 
-Did not implement timeline because the twitter documentation does not work for now
+Did not implement timeline because the twitter documentation does not work for this api for now
+
+## Setup the project
+Install the packages:
+```bash
+composer install
+```
+Database migration:
+```bash
+php artisan migrate
+```
---
+++ tests/Feature/TweetTest.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace Tests\Feature;
+
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TweetTest extends TestCase
+{
+    /**
+     * Test case for post a tweet
+     *
+     * @return void
+     */
+    public function testPost()
+    {
+        $response = $this->post('/api/post', ['status' => 'hello', 'user_id' => 1]);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test case for reply a tweet
+     *
+     * @return void
+     */
+    public function testReply()
+    {
+        $response = $this->post('/api/reply', ['status' => 'hello', 'tweet_id' => '1322780658154921984']);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test case for post a tweet
+     *
+     * @return void
+     */
+    public function testRetweet()
+    {
+        $response = $this->post('/api/retweet', ['tweet_id' => '1322780658154921984']);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test case for post a tweet
+     *
+     * @return void
+     */
+    public function testLike()
+    {
+        $response = $this->post('/api/like', ['tweet_id' => '1322780658154921984']);
+        $a=0;
+        $response->assertStatus(200);
+    }
+}
